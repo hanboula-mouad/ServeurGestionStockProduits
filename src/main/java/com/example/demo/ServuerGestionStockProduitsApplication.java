b8a4ce9ad2b147be4e1fef4dfae44c07ba02b281
unknown <mouad.hanboula@42consulting.ma>
2020-01-16 11:52:08 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServuerGestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServuerGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServuerGestionStockProduitsApplication.class, args);
+	}
+
+}
